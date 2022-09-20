fea12e4d56c520e16c1295b4e5f6aacdda3ae1b3
양현화 <hh.yang@fssuniverse.com>
2022-09-20 13:35:32 +0900

    video title 가져오기

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/VideoList';
 
 function App() {
-  return <h1>hello :)</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA0u3LlFYH5HIkZCiqS5zSCzisQsCj4j6Q", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />
 }
 
 export default App;
